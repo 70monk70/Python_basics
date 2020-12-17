b417eda312f0256ae3edc42206f946718737b426
monk <monk_aka_axat@mail.ru>
2020-12-17 16:13:26 +0300

    ок

--- Homework 2/task 4.py
+++ Homework 2/task 4.py
@@ -2,5 +2,5 @@ answer = input('Enter a string of several words: ')
 answer = answer.split(' ')
 i = 0
 for ind, el in enumerate(answer, 1):
-    print(ind, el) if len(answer[i]) <= 10 else print(ind, el[:10:])
-    i += 1
+    print(ind, el[:10:])
+    # print(ind, el) if len(answer[ind]) <= 10 else print(ind, el[:10:])
